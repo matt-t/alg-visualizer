7caadcd629bd8401dc4846f01ef2f9d1eff96b91
indancisive <47584841+indancisive@users.noreply.github.com>
2020-01-18 15:45:56 -0500

    edits

---
+++ src/components/board.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class board extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>hi griefer</h1>
+            </div>
+        );
+    }
+};
+
+export default board;
