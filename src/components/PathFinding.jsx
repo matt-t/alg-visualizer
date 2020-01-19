590d2b4e46350947b9cd1577bcd571b77cec626d
indancisive <47584841+indancisive@users.noreply.github.com>
2020-01-19 02:58:43 -0500

    fixes

--- src/components/PathFinding.jsx
+++ src/components/PathFinding.jsx
@@ -20,133 +20,6 @@ const getInitialGrid = () => {
   return grid;
 };
 
-const wallCheck = (grid, col, row) => {
-  if (row > 49 || row < 0 || col > 24 || col < 0) {
-    return true;
-  } else if (grid[row][col].isBlocked === false) {
-    return false;
-  } else if (grid[row][col].isWall === true) {
-    return true;
-  } else return false;
-};
-
-const weightCheck = (grid, row, col) => {
-  if (grid[row][col].isBlocked === false) {
-    return false;
-  } else if (grid[row][col].isWall === false) {
-    return true;
-  } else return false;
-};
-
-const neighborValues = [
-  [1, 0],
-  [0, 1],
-  [-1, 0],
-  [0, -1]
-];
-
-nodesToCheck = [];
-
-
-const addUnvisited = (grid, row, col, dist, heapCurrent) => {
-  if (heapCurrent == nodesToCheck.length) {
-    nodesToCheck.push([dist, grid[row][col]]);
-  } else if (nodesToCheck[heapCurrent][0] == dist) {
-    nodesToCheck[heapCurrent].push(grid[row][col]);
-  } else if (nodesToCheck[heapCurrent][0] > dist) {
-    nodesToCheck.splice([heapCurrent], 0, [dist, grid[row][col]]);
-  } else {
-    addUnvisited(grid, row, col, dist, heapCurrent + 1);
-  }
-  const node = grid[row][col];
-  const newNode = {
-    ...node,
-    distance: dist
-  };
-  grid[row][col] = newNode;
-};
-
-function deleteNode(nodesToCheck, row, col) {
-  for (heap in nodesToCheck) {
-    for (node in nodesToCheck[heap]) {
-      if (nodesToCheck[heap][node][0] == row && nodesToCheck[heap][node][1] == col) {
-        nodesToCheck = nodesToCheck.splice(nodesToCheck, 1)
-      }
-    }
-  } return nodesToCheck
-}
-
-const getNeighbors = (grid, row, col) => {
-  tempNeighbors = [];
-  for (direction in neighborValues) {
-    tempCoordinate = [row + direction[0], col + direction[1]];
-    tempNeighbors.push(tempCoordinate);
-  }
-  for (neighbor in tempNeighbors) {
-    finalNeighbors = [];
-    if (
-      wallCheck(grid, tempNeighbors[neighbor][0], tempneighbor[neighbor][1]) ===
-      false
-    ) {
-      finalNeighbors.push([
-        tempNeighbors[neighbor][0],
-        tempneighbor[neighbor][1]
-      ]);
-    }
-  }
-  return finalNeighbors;
-};
-
-const changeDistance = (grid, row, col) => {};
-const weight = 3;
-
-function getClosestNode(nodesToCheck) {
-  if (nodesToCheck.length == 0) {
-    return empty
-  } else (nodesToCheck[0][0])
-}
-
-function getAllNodes
-
-export function dijkstra(grid, startRow, startCol, endRow, endCol) {
-  var currentRow = startRow;
-  var currentCol = startCol;
-  newGrid = grid;
-  while (this.state.done === false) {
-    currentNeighbors = getNeighbors(grid, currentRow, currentCol);
-    for (neighbor in currentNeighbors) {
-      if (weightCheck(newGrid, currentNeighbors[neighbor][0], currentNeighbors[neighbor][1])) {
-        nodesToCheck = deleteNode(nodesToCheck, currentNeighbors[neighbor][0], currentNeighbors[neighbor][1]);
-        addUnvisited(
-          newGrid,
-          currentNeighbors[neighbor][0],
-          currentNeighbors[neighbor][1],
-          grid[currentRow][currentCol].distance + weight,
-          0
-        );
-      } else
-        nodesToCheck = deleteNode(nodesToCheck, currentNeighbors[neighbor][0], currentNeighbors[neighbor][1]);
-        addUnvisited(
-          newGrid,
-          currentNeighbors[neighbor][0],
-          currentNeighbors[neighbor][1],
-          grid[currentRow][currentCol].distance + 1,
-          0
-        );
-    }
-    this.setState({ grid: newGrid });
-  }
-};
-
-
-var unvisitedNodes = [];
-var visitedNodes = [];
-var currentNode = [];
-
-const dijkstra = grid => {
-  currentNode.push(grid[START_NODE_ROW][START_NODE_COL]);
-};
-
 const createNode = (col, row) => {
   return {
     col,
@@ -189,26 +62,17 @@ export default class PathFinding extends Component {
   constructor() {
     super();
     this.state = {
-<<<<<<< HEAD
-      grid: []
-=======
       grid: [],
       mouseIsPressed: false,
       message: "Mouse Event",
       addingWalls: true
->>>>>>> branch-1
     };
   }
 
   componentDidMount() {
     const grid = getInitialGrid();
-<<<<<<< HEAD
     this.setState({ grid: grid });
     console.log(grid[0][0]);
-=======
-    console.log(grid[0][0]);
-    this.setState({ grid: grid });
->>>>>>> branch-1
   }
   /*
   clap = (grid, row, col) => {
@@ -223,31 +87,6 @@ export default class PathFinding extends Component {
   }
   */
 
-<<<<<<< HEAD
-  render() {
-    const { grid } = this.state;
-    return (
-      <div className="grid">
-        {grid.map((row, rowIdx) => {
-          return (
-            <div key={rowIdx}>
-              {row.map((node, nodeIdx) => {
-                const { row, col, isFinish, isStart, isWall } = node;
-                return (
-                  <Cell
-                    key={nodeIdx}
-                    col={col}
-                    isFinish={isFinish}
-                    isStart={isStart}
-                    isWall={isWall}
-                    row={row}
-                  ></Cell>
-                );
-              })}
-            </div>
-          );
-        })}
-=======
   handleMouseDown(row, col) {
     // console.log(row)
     // console.log(col)
@@ -356,7 +195,6 @@ export default class PathFinding extends Component {
             );
           })}
         </div>
->>>>>>> branch-1
       </div>
     );
   }
--- src/components/algorithm.js
+++ src/components/algorithm.js
@@ -1,46 +1,3 @@
-const wallCheck = node => {
-  if (node.row > 49 || node.row < 0 || node.col > 24 || node.col < 0) {
-    return true;
-  } else if (node.isWall === true) {
-    return true;
-  } else return false;
-};
-
-const neighborValues = [
-  [1, 0],
-  [0, 1],
-  [-1, 0],
-  [0, -1]
-];
-
-function sortNodes(unvisitedNodes) {
-  unvisitedNodes.sort((a, b) => a.distance - b.distance);
-}
-
-const getNeighbors = (grid, node) => {
-  const tempNeighbors = [];
-  for (const direction in neighborValues) {
-    const tempNode = grid[node.row + direction[0]][node.col + direction[1]];
-    if (tempNode.isVisited === false && tempNode.isWall === false) {
-      tempNeighbors.push(tempNode);
-    }
-    return tempNeighbors;
-  }
-};
-const weight = 3;
-
-function getAllNodes(grid) {
-  const nodeArr = [];
-  for (row of grid) {
-    for (node of row) {
-      if (node.isWall === false) {
-        nodeArr.push(node);
-      }
-    }
-  }
-  return nodeArr;
-}
-
 export function dijkstra(grid, startNode) {
   var solved = false;
   const visitedNodes = [];
@@ -84,4 +41,47 @@ export function getNodesInShortestPathOrder(finishNode) {
     currentNode = currentNode.previousNode;
   }
   return nodesInShortestPathOrder;
+}
+
+const wallCheck = node => {
+  if (node.row > 49 || node.row < 0 || node.col > 24 || node.col < 0) {
+    return true;
+  } else if (node.isWall === true) {
+    return true;
+  } else return false;
+};
+
+const neighborValues = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1]
+];
+
+function sortNodes(unvisitedNodes) {
+  unvisitedNodes.sort((a, b) => a.distance - b.distance);
+}
+
+const getNeighbors = (grid, node) => {
+  const tempNeighbors = [];
+  for (const direction in neighborValues) {
+    const tempNode = grid[node.row + direction[0]][node.col + direction[1]];
+    if (tempNode.isVisited === false && tempNode.isWall === false) {
+      tempNeighbors.push(tempNode);
+    }
+    return tempNeighbors;
+  }
+};
+const weight = 3;
+
+function getAllNodes(grid) {
+  const nodeArr = [];
+  for (row of grid) {
+    for (node of row) {
+      if (node.isWall === false) {
+        nodeArr.push(node);
+      }
+    }
+  }
+  return nodeArr;
 }
